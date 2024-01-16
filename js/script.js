6954877096dae17ad7edd9652fa5f0778eabe25d
muchammadwr <muchammadwr@gmail.com>
2024-01-16 20:44:26 +0700

    Menambah Testimonials

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ const menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
   menu.classList.toggle("move");
 };
+
+// Swiper
+
+var swiper = new Swiper(".services-container", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
